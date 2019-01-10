678e000c080368733bbdea2353970188691fc65b
Tahir H. Butt <tahir.butt@datadoghq.com>
2019-01-10 16:07:57 -0500

    Cut out unused modules and configs

--- config.el
+++ config.el
@@ -3,12 +3,12 @@
 (setq-default
       user-mail-address "tahir@tahirbutt.com"
       user-full-name    "Tahir H. Butt"
-      doom-font (font-spec :family "Input Mono Narrow" :size 14 :weight 'semi-light)
-      doom-variable-pitch-font (font-spec :family "Input Sans Narrow" :size 14 :weight 'normal)
-      doom-unicode-font (font-spec :family "Sarasa Mono SC" :size 12 :weight 'normal)
-      doom-big-font (font-spec :family "Input Mono Narrow" :size 18 :weight 'semi-light)
-      ovp-font "Iosevka Term"
-      doom-theme 'doom-city-lights)
+      doom-font (font-spec :family "Iosevka" :size 14)
+      doom-serif-font (font-spec :family "Iosevka Slab" :size 14)
+      doom-unicode-font (font-spec :family "Iosevka")
+      doom-big-font (font-spec :family "Iosevka" :size 28)
+      doom-variable-pitch-font (font-spec :family "Input Sans Condensed")
+      doom-theme (if (display-graphic-p) 'doom-Iosvkem nil))
 
 (add-to-list 'default-frame-alist
              '(ns-transparent-titlebar . t))
@@ -16,37 +16,10 @@
 (add-to-list 'default-frame-alist
              '(ns-appearance . dark))
 
-(after! company
-  (setq company-idle-delay 0.1
-        company-minimum-prefix-length 2
-        company-quickhelp-mode t
-        company-quickhelp-delay 0.4
-        company-backends '((company-yasnippet
-                            company-keywords
-                            company-capf))))
-
-;; lang/org
-(setq org-directory (expand-file-name "~/Dropbox/org/")
-      org-agenda-files (list org-directory)
-      org-ellipsis " ▼ "
-      org-bullets-bullet-list '("#"))
-
-(after! ox-pandoc
-  (setq org-pandoc-options
-        '((standalone . t)
-          (mathjax . t)))
-  (setq org-pandoc-options-for-latex-pdf
-        '((pdf-engine . "xelatex"))))
-
 (after! org
-  (setq +org-babel-mode-alist
-        '(("ipython" . ipython)
-          ("bash" . shell)
-          ("plantuml" . t)
-          ("sh" . shell)))
-
-  (setq org-plantuml-jar-path
-      (expand-file-name "/opt/plantuml/plantuml.jar"))
+  (setq org-directory (expand-file-name "~/Dropbox/org/")
+        org-agenda-files (list org-directory)
+        org-ellipsis " ▼ ")
 
   (setq org-capture-templates
         '(("t" "Todo" entry
@@ -59,37 +32,7 @@
 
           ("n" "Notes" entry
            (file+headline "notes.org" "Inbox")
-           "* %u %?\n %i" :prepend t :kill-buffer t)
-
-          ("dn" "Dissertation: Journal" entry
-           (file+olp+datetree "dissertation.org" "Journal")
-           "* %? :note:\n %i" :prepend t :kill-buffer t)
-
-          ("dt" "Dissertation: Todo" entry
-           (file+headline "dissertation.org" "Todo")
-           "* TODO %?\n %i\n %a" :prepend t :kill-buffer t)
-
-          ("ddn" "Datadog: Journal" entry
-           (file+olp+datetree "datadog.org" "Journal")
-           "* %? :note:\n %i" :prepend t :kill-buffer t)
-
-          ("ddt" "Datadog: Todo" entry
-           (file+headline "datadog.org" "Todo")
-           "* TODO %?\n %i\n %a" :prepend t :kill-buffer t))))
-
-(after! elfeed-show
-  (map! (:map elfeed-show-mode-map
-          [remap kill-this-buffer]      "q"
-          [remap kill-buffer]           "q"
-          :nm "q"   #'+rss/delete-pane
-          :nm "o"   #'ace-link-elfeed
-          :nm "RET" #'org-ref-add-bibtex-entry-from-elfeed-entry
-          :nm "n"   #'elfeed-show-next
-          :nm "p"   #'elfeed-show-prev
-          :nm "+"   #'elfeed-show-tag
-          :nm "-"   #'elfeed-show-untag
-          :nm "s"   #'elfeed-show-new-live-search
-          :nm "y"   #'elfeed-show-yank)))
+           "* %u %?\n %i" :prepend t :kill-buffer t))))
 
 (after! tramp-sh
   (setq tramp-default-method "ssh"
